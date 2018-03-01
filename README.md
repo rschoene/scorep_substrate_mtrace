5d3672c2ef121a5ede48b1a9a33b798a6013360e
Robert Schöne <robert.schoene@tu-dresden.de>
2018-03-01 09:20:51 +0100

    source and readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # scorep_substrate_mtrace
 Enables mtrace to detect memory leaks
 
-# Compile
+# Compile and Install
+
+Just run `make` within the `src` directory and copy the plugin (`libscorep_substrate_mtrace.so` to a path in `$LD_LIBRARY_PATH`)
 
 # Usage
+- Make sure `libscorep_substrate_mtrace.so` is accessible in the `$LD_LIBRARY_PATH`
+- `export SCOREP_SUBSTRATE_MTRACE_FILE=(file-prefix)` will create files named `(file-prefix).(mpp_rank)`. For example if it is set to `/tmp/mem_test` and you run a test with 4 MPI ranks, there will be 4 files: `/tmp/mem_test.[0-3]`
+- if `$SCOREP_SUBSTRATE_MTRACE_FILE` is not set, an error will be printed and `$MALLOC_TRACE` will be used
---
+++ src/scorep_substrate_mtrace.c
@@ -0,0 +1,61 @@
+/*
+ * scorep_substrate_mtrace.c
+ *
+ *  Created on: 01.03.2018
+ *      Author: rschoene
+ */
+
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <mcheck.h>
+
+#include <scorep/SCOREP_SubstratePlugins.h>
+
+#define PREFIX "SCOREP_SUBSTRATE_MTRACE"
+
+
+/* Callback functions from Score-P */
+static const SCOREP_SubstratePluginCallbacks* callbacks;
+
+/* sets the Score-P callbacks */
+static void
+substrate_mtrace_set_callbacks (
+    const SCOREP_SubstratePluginCallbacks* callbacks_in, size_t size)
+{
+  assert(size >= sizeof(SCOREP_SubstratePluginCallbacks));
+  callbacks = callbacks_in;
+}
+
+/* Called when MPP is initialized (e.g., when MPI_Init() is called) or after init when no MPP is used */
+static void
+substrate_mtrace_init_mpp (void)
+{
+  /* Get the metrics from env. variable */
+  char* env_metrics = getenv (PREFIX"_FILE");
+  if (env_metrics)
+  {
+    char buffer[512];
+    sprintf(buffer,"%s.%d",env_metrics,callbacks->SCOREP_Ipc_GetRank());
+    setenv("MALLOC_TRACE", env_metrics, 0);
+  }
+  else
+  {
+    if ( callbacks->SCOREP_Ipc_GetRank() == 0 )
+      fprintf(stderr,PREFIX"_FILE is not set, $MALLOC_TRACE will be used\n");
+  }
+  mtrace();
+}
+
+
+/* define the plugin */
+SCOREP_SUBSTRATE_PLUGIN_ENTRY (print_metrics)
+{
+  SCOREP_SubstratePluginInfo info;
+  memset (&info, 0, sizeof(SCOREP_SubstratePluginInfo));
+  info.plugin_version = SCOREP_SUBSTRATE_PLUGIN_VERSION;
+  info.init_mpp = substrate_mtrace_init_mpp;
+  info.set_callbacks = substrate_mtrace_set_callbacks;
+  return info;
+}
